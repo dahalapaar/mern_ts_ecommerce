762028da8b6dc12d2876d17e5059841150e5a801
dahalapaar <contact.apaardahal@gmail.com>
2023-12-30 21:46:36 +1100

    product component created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
 import "./App.css"
+import { sampleProducts } from "./data"
 
 function App() {
   return (
     <>
       <header>Ecommerce</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img className="product-image" src={product.image} />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All rights reserved @Apaar</footer>
     </>
   )
---
+++ frontend/src/data.ts
@@ -0,0 +1,68 @@
+import { Product } from "./types/Product"
+
+export const sampleProducts: Product[] = [
+  {
+    name: "Nike",
+    slug: "nike-slim-shirt",
+    category: "Shirts",
+    image: "../images/p1.jpg",
+    price: 120,
+    countInstock: 10,
+    brand: "Nike",
+    rating: 5,
+    numReviews: 12,
+    description: "Good Quality",
+  },
+
+  {
+    name: "Nike",
+    slug: "nike-slim-pant",
+    category: "Shirts",
+    image: "../images/p2.jpg",
+    price: 120,
+    countInstock: 10,
+    brand: "Nike",
+    rating: 5,
+    numReviews: 12,
+    description: "Good Quality",
+  },
+
+  {
+    name: "Nike",
+    slug: "nike-slim",
+    category: "Shirts",
+    image: "../images/p3.jpg",
+    price: 120,
+    countInstock: 10,
+    brand: "Nike",
+    rating: 5,
+    numReviews: 12,
+    description: "Good Quality",
+  },
+
+  {
+    name: "Nike",
+    slug: "nike-slim-t",
+    category: "Shirts",
+    image: "../images/p4.jpg",
+    price: 120,
+    countInstock: 10,
+    brand: "Nike",
+    rating: 5,
+    numReviews: 12,
+    description: "Good Quality",
+  },
+
+  {
+    name: "Nike",
+    slug: "nike-slim-p",
+    category: "Shirts",
+    image: "../images/p1.jpg",
+    price: 120,
+    countInstock: 10,
+    brand: "Nike",
+    rating: 5,
+    numReviews: 12,
+    description: "Good Quality",
+  },
+]
