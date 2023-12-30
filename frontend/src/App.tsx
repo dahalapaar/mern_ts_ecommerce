64daff560e1857f2361473804322382dc362781d
dahalapaar <contact.apaardahal@gmail.com>
2023-12-30 21:00:54 +1100

    ecommerce project template

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <header>Ecommerce</header>
+      <main></main>
+      <footer>All rights reserved @Apaar</footer>
+    </>
+  )
+}
+
+export default App
