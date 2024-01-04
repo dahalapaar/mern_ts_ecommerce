8af012b63ea845d5e14ea6ca3197cae4db3610a4
dahalapaar <contact.apaardahal@gmail.com>
2024-01-04 21:17:56 +1100

    feat: types defination

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,40 @@
 import { Col, Row } from "react-bootstrap"
 import { sampleProducts } from "../data"
 import { Link } from "react-router-dom"
+import { Product } from "../types/Product"
+
+type State = {
+  products: Product[]
+  loading: boolean
+  error: string
+}
+
+type Action =
+  | { type: "FETCH_REQUEST" }
+  | {
+      type: "FETCH_SUCCESS"
+      payload: Product[]
+    }
+  | { type: "FETCH_FAIL"; payload: string }
+
+const initialState: State = {
+  products: [],
+  loading: true,
+  error: "",
+}
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true }
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false }
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
 
 const HomePage = () => {
   return (
